b69bd860d879328abfa96455e8fefa44fbfe5355
HubMaster123 <162082708+HubMaster123@users.noreply.github.com>
2024-03-25 16:28:40 +1100

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "LIAM"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "liam2002"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2474376			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "FJT90AA4C9FYEVX6"   // replace XYZ with your channel write API Key
